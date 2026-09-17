730a629c726771674cd4900821213030c804db57
Sushmita2k04 <sushmita2k04@gmail.com>
2026-09-17 23:13:36 +0530

    Revert "all the design for modules"
    This reverts commit 845b287e2de80d9180a11d30e0f9e878791eb85c.

--- app/design/frontend/Codilar/custom/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            customHeader: 'js/custom-header',
-            headerSearch: 'js/header-search',
-            cartQtyUpdater: 'js/cart-qty-updater'
-        }
-    }
-};
